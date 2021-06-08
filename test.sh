e6809a8248c51859a4e219484571b75a32b580f5
Tom Smeding <tom@tomsmeding.com>
2021-06-08 12:13:59 +0200

    Add env vars for jizo in test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+if [[ "$(hostname)" = jizo ]]; then
+	echo "Entering environment for jizo"
+	export LD_LIBRARY_PATH="/usr/local/cuda-10.1/nvvm/lib64/"
+	export PATH="/usr/local/cuda-10.1/bin:$PATH"
+fi
+
 stack build
 while true; do
 	cuda-memcheck stack run || break
